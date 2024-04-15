384390a79de91dae0480a987eaafe9f05de47421
ompharate31 <ompharate31@gmail.com>
2024-04-15 10:11:34 +0530

    made project 1 responsive

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <BrowserRouter>
-    <Header/>
+    {/* <Header/> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<About />} />
